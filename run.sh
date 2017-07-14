44ff76e56e3f630ec41e5b662214f9a3c3050991
Alex Thewsey <athewsey@cantab.net>
2017-07-14 23:39:40 +0100

    JWT test

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ python3 -m venv pyenv
 pyenv/bin/pip install aiohttp
 pyenv/bin/pip install asyncio
 pyenv/bin/pip install cchardet
+pyenv/bin/pip install pyjwt
 
 pyenv/bin/python3 server
--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,31 @@
 #! /usr/bin/env python3
 from aiohttp import web
+from datetime import datetime, timedelta
+import jwt
+import os
 
-async def handle(req):
+JWT_SECRET = "secret"
+JWT_ALGORITHM = "HS512"
+JWT_EXPIRY_SECONDS = 20
+
+async def login(req):
+    post_data = await req.post()
+    try:
+        assert post_data["id"] == "user"
+        assert post_data["password"] == "password"
+    except (KeyError, AssertionError):
+        return web.json_response(
+            { "message": "Incorrect credentials" },
+            status=400
+        )
+    payload = {
+        "id": "user",
+        "expires": (datetime.utcnow() + timedelta(seconds=JWT_EXPIRY_SECONDS)).isoformat()
+    }
+    jwt_token = jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
+    return web.json_response({ "token": jwt_token.decode("utf-8") })
+
+async def test(req):
     name = "Anonymous"
     try:
         name = req.query["name"]
@@ -11,6 +35,12 @@ async def handle(req):
     return web.json_response(text)
 
 app = web.Application()
-app.router.add_get("/", handle)
+
+app.router.add_post("/login", login)
+app.router.add_get("/test", test)
+app.router.add_static(
+    "/",
+    os.path.abspath(os.path.join("client"))
+)
 
 web.run_app(app)
